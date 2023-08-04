6e409eb4c240cc8a88dd671f4b647ae9b6bad5c6
Gustavo Amora <gustavoamora@gmail.com>
2023-08-04 15:30:17 -0300

    Update sql_code.sql

--- proj_2/sql_code.sql
+++ proj_2/sql_code.sql
@@ -1,32 +1,32 @@
-WITH ultimo_mes AS (
+WITH last_month AS (
   SELECT 
-      database_name,
-      SUM(mt_total) AS mt_total_mes_anterior
+      customer_name,
+      SUM(mt_total) AS total_last_month
   FROM
-      `wavy-business-analytics.big_numbers_sms.SMS_OLAP`
+      my_table
   WHERE
       message_date >= DATE_SUB(DATE_TRUNC(CURRENT_DATE(), MONTH), INTERVAL 1 MONTH) 
       AND message_date < DATE_TRUNC(CURRENT_DATE(), MONTH) 
   GROUP BY
-      database_name
+      customer_name
 )
 
 SELECT 
-    v.database_name,
+    v.customer_name,
     v.carrier_name,
     MAX(v.message_date) AS message_date,
     SUM(v.mt_total) AS mt_total,
-    ROUND(100 * SUM(v.mt_total) / t.mt_total_mes_anterior, 2) AS percentual_volume
+    ROUND(100 * SUM(v.mt_total) / t.total_last_month, 2) AS percentual_volume
 FROM
-    `wavy-business-analytics.big_numbers_sms.SMS_OLAP` v
+    my_table v
 JOIN
-    TotaisMesAnterior t
+    last_month t
 ON
-    v.database_name = t.database_name
+    v.customer_name = t.customer_name
 WHERE
-    v.message_date >= DATE_SUB(DATE_TRUNC(CURRENT_DATE(), MONTH), INTERVAL 1 MONTH) -- primeiro dia do mês anterior
-    AND v.message_date < DATE_TRUNC(CURRENT_DATE(), MONTH) -- primeiro dia do mês atual
+    v.message_date >= DATE_SUB(DATE_TRUNC(CURRENT_DATE(), MONTH), INTERVAL 1 MONTH) 
+    AND v.message_date < DATE_TRUNC(CURRENT_DATE(), MONTH) 
 GROUP BY
-    v.database_name,
+    v.customer_name,
     v.carrier_name,
-    t.mt_total_mes_anterior;
+    t.total_last_month;
